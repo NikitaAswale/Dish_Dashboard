14feb3ac24fd06b31c51bf348f50b4bcc2f3a091
NikitAswale <aswalenikita26@gmail.com>
2025-07-13 18:37:29 +0530

    Updated UI

--- app/src/main/java/com/example/dishdashboard/DishDashboard.kt
+++ app/src/main/java/com/example/dishdashboard/DishDashboard.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -25,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -33,106 +35,117 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun DishDashboard(){
+fun DishDashboard() {
 
     var text by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("")}
+    var password by remember { mutableStateOf("") }
 
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().
+        background(Brush.verticalGradient(colors = listOf(Color(0xFF1D2B64), Color(0xFF4DA0B0))))
         //contentAlignment = Alignment.Center,
-    )
 
-    Column (modifier = Modifier.fillMaxSize().padding(4.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        Spacer(modifier = Modifier.height(12.dp))
+    ) {
 
-        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "",
-            modifier = Modifier.size(10.dp).clip(shape = CircleShape)
+        Column(
+            modifier = Modifier.fillMaxSize().padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "",
+                modifier = Modifier.size(100.dp).clip(shape = CircleShape)
             )
 
-        Spacer(modifier = Modifier.height(6.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Text("DishDashboard",
-            modifier = Modifier,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            fontStyle = FontStyle.Normal,
-            color = Color.White)
+            Text(
+                "DishDashboard",
+                modifier = Modifier,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Normal,
+                color = Color.White
+            )
 
-        Text("Restaurant Management Made Easy",
-            modifier = Modifier,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.SemiBold,
-            fontStyle = FontStyle.Normal,
-            color = Color.White)
-    }
+            Text(
+                "Restaurant Management Made Easy",
+                modifier = Modifier,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontStyle = FontStyle.Normal,
+                color = Color.White
+            )
 
-    Spacer(modifier = Modifier.height(8.dp))
-        Card (
-            modifier = Modifier,
+        Spacer(modifier = Modifier.height(20.dp))
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(20.dp),
             elevation = CardDefaults.cardElevation(26.dp)
-        ){
+        ) {
             Spacer(modifier = Modifier.height(4.dp))
 
-            Column (
+            Column(
                 modifier = Modifier.fillMaxWidth().padding(8.dp),
                 //verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
-                ){
+            ) {
 
-            Text("Restaurant Management Made Easy",
-                modifier = Modifier,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                fontStyle = FontStyle.Normal,
-                color = Color.Blue)
+                Text(
+                    "Welcome Back",
+                    modifier = Modifier,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Normal,
+                    color = Color.Blue
+                )
 
-            Spacer(modifier = Modifier.height(6.dp))
+                Spacer(modifier = Modifier.height(6.dp))
 
-            OutlinedTextField(
-                value = text,
-                onValueChange = {password = it},
-                label = {
-                    Text(
-                        "Username",
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 12.sp,
-                        color = Color.DarkGray
-                    )
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = { password = it },
+                    label = {
+                        Text(
+                            "Username",
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 12.sp,
+                            color = Color.DarkGray
+                        )
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(6.dp))
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = {
+                        Text(
+                            "Password",
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 12.sp,
+                            color = Color.DarkGray
+                        )
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(6.dp))
+
+                Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+
+                    Text("Sign In ->")
+
                 }
-            )
 
-            Spacer(modifier = Modifier.height(6.dp))
+                Spacer(modifier = Modifier.height(10.dp))
 
-            OutlinedTextField(
-                value = password,
-                onValueChange = {password = it},
-                label = {
-                    Text(
-                        "Password",
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 12.sp,
-                        color = Color.DarkGray
-                    )
-                }
-            )
-
-            Spacer(modifier = Modifier.height(6.dp))
-
-            Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
-
-                Text("Sign In ->")
-
+                Text("Forgot Password?")
             }
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Text("Forgot Password?")
-
+        }
         }
     }
-    }
+}
